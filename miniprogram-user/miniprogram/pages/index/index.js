39f3c3db3d8f7ca3ffa342957b4f57732e6c418b
sad-wang <leown_wang@163.com>
2020-02-18 00:44:45 +0800

    added getUserFavoritesCount function

--- miniprogram-user/miniprogram/pages/index/index.js
+++ miniprogram-user/miniprogram/pages/index/index.js
@@ -1,11 +1,13 @@
 //index.js
 const app = getApp()
+const db = wx.cloud.database()
 Page({
   data: {
     avatarUrl: '../../images/user-unlogin.png',
     userInfo: {},
     logged: false,
     recommendLists:[],
+    count:''
   },
   navTo(url){
     wx.navigateTo({
@@ -15,6 +17,7 @@ Page({
   onLoad: function() {
     this.getKnownUserInfo()
     this.getRecommendLists()
+    this.getUserFavoritesCount()
   },
   getKnownUserInfo:function() {
     wx.getSetting({
@@ -40,7 +43,23 @@ Page({
       })
     }.bind(this))
   },
-
+  // aa:function(){
+  //   db.collection('user').add({
+  //     data: {
+  //       favorites:["a8bafbad-5f36-442c-b7ec-d22f73ef8f9a"]
+  //     },
+  //     success: function(res) {
+  //       console.log(res)
+  //     }
+  //   })
+  // },
+  getUserFavoritesCount:function(){
+    db.collection('user').where({_openid: app.globalData.openid}).get().then(function(res){
+      this.setData({
+        count: res.data[0].favorites.length
+      })
+    }.bind(this))
+  },
   onGetUserInfo: function(e) {
     if (!this.data.logged && e.detail.userInfo) {
       this.setData({
