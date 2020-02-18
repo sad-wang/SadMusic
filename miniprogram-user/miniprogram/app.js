4c63243575d660eed1c49eea428c5bbd12977ed0
sad-wang <leown_wang@163.com>
2020-02-18 16:53:52 +0800

    optimized playing style sheet

--- miniprogram-user/miniprogram/app.js
+++ miniprogram-user/miniprogram/app.js
@@ -1,23 +1,5 @@
 //app.js
 App({
-  eventHub:{
-    events:{},
-    emit(eventName,data){
-      for (let key in this.events){
-        if (key === eventName){
-          let fnList = this.events[key];
-          fnList.map((fn)=>{
-            fn.call(undefined,data)
-          })
-        }
-      }
-    },
-    on(eventName,fn){
-      if (this.events[eventName] === undefined)
-        this.events[eventName] = []
-      this.events[eventName].push(fn)
-    },
-  },
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
