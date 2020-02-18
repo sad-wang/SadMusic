b0903c662206a113f34dba3da246506297c07680
sad-wang <leown_wang@163.com>
2020-02-19 05:41:45 +0800

    achieved playing function

--- miniprogram-user/miniprogram/components/bottomBar/bottomBar.js
+++ miniprogram-user/miniprogram/components/bottomBar/bottomBar.js
@@ -1,4 +1,5 @@
 // components/bottomBar.js
+let audio = wx.createInnerAudioContext()
 Component({
   properties: {
     detail:{
@@ -7,32 +8,62 @@ Component({
   },
   data: {
     songLists:[],
-    index:'',
+    index:null,
     way:'',
+    state:null
   },
   lifetimes: {
-    attached: function () {
-      let audio = wx.createInnerAudioContext()
-      audio.src='cloud://sad-music.7361-sad-music-1301301671/songs/朴树-她在睡梦中.mp3'
-      audio.play()
-      audio.pause()
-    },
+    attached: function () {},
   },
   pageLifetimes: {
-    // 组件所在页面的生命周期函数
     show: function () { },
   },
   methods: {
     updatePlaying(){
       console.log(1)
+    },
+    pause(){
+      if(this.data.state){
+        audio.pause()
+        this.setData({
+          state:0
+        })
+      } else {
+        audio.play()
+        this.setData({
+          state:1
+        })
+      }
+    },
+    previous(){
+      audio.destroy()
+      audio = wx.createInnerAudioContext()
+      this.setData({
+        index:((this.data.index-1)+this.data.songLists.length)%this.data.songLists.length
+      })
+      audio.src=this.data.songLists[this.data.index].url
+      audio.play()
+    },
+    next(){
+      audio.destroy()
+      audio = wx.createInnerAudioContext()
+      this.setData({
+        index:(this.data.index+1)%this.data.songLists.length
+      })
+      audio.src=this.data.songLists[this.data.index].url
+      audio.play()
     }
   },
   observers: {
     'detail': function() {
       this.setData({
-        ...this.data.detail
+        ...this.data.detail,
+        state:1
       })
+      audio.destroy()
+      audio = wx.createInnerAudioContext()
+      audio.src = this.data.songLists[this.data.index]?this.data.songLists[this.data.index].url:null
+      audio.play()
     }
-
   }
 })
